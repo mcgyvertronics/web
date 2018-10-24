2f9444fea0ed1c15afff9670172477c210b1c847
Luiz <luizhrios@gmail.com>
2018-10-24 09:26:45 -0400

    Projects Final

--- src/app/maorobotica/maorobotica.component.ts
+++ src/app/maorobotica/maorobotica.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaoRoboticaComponent implements OnInit {
 
-  descricao = 'Mão Robótica - Mão robótica humanoide, destinada a proporcionar inclusão e acessibilidade a portadores de deficiência que não tenha as mãos. Em processo de desenvolvimento, espera-se ao final das pesquisas e desenvolvimento que seja capaz de captar sinais eletromiográficos dos nervos residuais. No momento controlada por acionamento manual.';
-
   constructor() { }
 
   ngOnInit() {
